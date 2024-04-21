39964054d64b65a0cb20e20747f7809175eeb2a1
Camila <mendemmcamila@gmail.com>
2024-04-21 00:44:57 -0300

    feat: appbar component

--- src/components/appbar/index.tsx
+++ src/components/appbar/index.tsx
@@ -1,33 +1,24 @@
-import { Menu } from "@mui/icons-material";
-import {
-  Box,
-  IconButton,
-  Toolbar,
-  AppBar as MuiAppBar,
-  Typography,
-  Button,
-} from "@mui/material";
+"use client";
+import Logo from "@/assets/logo.png";
+import { Box, Grid, Toolbar } from "@mui/material";
+import Image from "next/image";
+import { Switch } from "../switch";
+import * as S from "./styles";
+import { useTheme } from "@/hooks/useTheme";
 
 export const AppBar: React.FC = () => {
+  const { toggleTheme } = useTheme();
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <MuiAppBar position="static" color="primary">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <Menu />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </MuiAppBar>
-    </Box>
+    <S.AppBar position="static">
+      <Grid container alignItems="center" justifyContent="space-between">
+        <Grid item>
+          <Image src={Logo} alt="logo" />
+        </Grid>
+        <Grid item>
+          <Switch onClick={toggleTheme} />
+        </Grid>
+      </Grid>
+    </S.AppBar>
   );
 };
---
+++ src/components/appbar/styles.ts
@@ -0,0 +1,8 @@
+"use client";
+import { AppBar as MuiAppBar, styled } from "@mui/material";
+
+export const AppBar = styled(MuiAppBar)(({ theme }) => ({
+  backgroundColor: theme.palette.grey[300],
+  height: "3rem",
+  padding: "0.25rem calc((100vw - 60rem)/2)",
+}));
